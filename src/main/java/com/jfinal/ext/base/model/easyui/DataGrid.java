f24431931088135d8fb674b876749655cd4b1923
huguangju <huguangjugit@163.com>
2014-07-21 01:26:10 +0800

    test pushed code whether or not view in github.com

--- src/main/java/com/jfinal/ext/base/model/easyui/DataGrid.java
+++ src/main/java/com/jfinal/ext/base/model/easyui/DataGrid.java
@@ -5,34 +5,30 @@ import java.util.List;
 
 /**
  * EasyUI DataGrid模型
+ * 
  * @author 孙宇
  */
 @SuppressWarnings("serial")
-public class DataGrid<T> implements java.io.Serializable
-{
+public class DataGrid<T> implements java.io.Serializable {
 	public int total = 0;
 	public int page;
 	public String sortName = "";
 	public String sortOrder = "";
 	public List<T> rows = new ArrayList<T>();
 
-	public int getTotal()
-	{
+	public int getTotal() {
 		return total;
 	}
 
-	public void setTotal(int total)
-	{
+	public void setTotal(int total) {
 		this.total = total;
 	}
 
-	public List<T> getRows()
-	{
+	public List<T> getRows() {
 		return rows;
 	}
 
-	public void setRows(List<T> rows)
-	{
+	public void setRows(List<T> rows) {
 		this.rows = rows;
 	}
 
